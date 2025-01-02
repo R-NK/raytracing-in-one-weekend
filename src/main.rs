45dcbf96fa21e86ed16749ad0b871fb9a69235f3
R-NK <right.rcon.f@outlook.com>
2025-01-03 05:49:43 +0900

    first raytraced image

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
 use ray::Ray;
-use vec3::Color;
+use vec3::{Color, Point3, Vec3};
 
 mod print;
 mod ray;
 mod vec3;
 
-fn ray_color(r: ray::Ray) -> vec3::Color {
+fn hit_sphere(center: Point3, radius: f64, r: &Ray) -> bool {
+    let oc = r.origin() - center;
+    let a = r.direction().dot(r.direction());
+    let b = 2.0 * oc.dot(r.direction());
+    let c = oc.dot(oc) - radius * radius;
+    let discriminant = b * b - 4.0 * a * c;
+    discriminant > 0.0
+}
+
+fn ray_color(r: ray::Ray) -> Color {
+    if hit_sphere(Point3::new_with_values(0.0, 0.0, -1.0), 0.5, &r) {
+        return Color::new_with_values(1.0, 0.0, 0.0);
+    }
     let unit_direction = r.direction().unit_vector();
     let t = 0.5 * (unit_direction.y() + 1.0);
     Color::new_with_values(1.0, 1.0, 1.0) * (1.0 - t) + Color::new_with_values(0.5, 0.7, 1.0) * t
@@ -22,11 +34,10 @@ fn main() {
     let viewport_width = ASPECT_RATIO * viewport_height;
     let focal_length = 1.0;
 
-    let origin = vec3::Point3::new();
-    let horizontal = vec3::Vec3::new_with_values(viewport_width, 0.0, 0.0);
-    let vertical = vec3::Vec3::new_with_values(0.0, viewport_height, 0.0);
-    let lower_left_corner =
-        origin - horizontal / 2.0 - vertical / 2.0 - vec3::Vec3::new_with_values(0.0, 0.0, focal_length);
+    let origin = Point3::new();
+    let horizontal = Vec3::new_with_values(viewport_width, 0.0, 0.0);
+    let vertical = Vec3::new_with_values(0.0, viewport_height, 0.0);
+    let lower_left_corner = origin - horizontal / 2.0 - vertical / 2.0 - Vec3::new_with_values(0.0, 0.0, focal_length);
 
     for j in (0..IMAGE_HEIGHT).rev() {
         eprintln!("\rScanlines remaining: {}", j);
