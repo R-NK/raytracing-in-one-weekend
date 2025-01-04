09e01120dbc92ae18ded46d92d3701d40b1fcab4
R-NK <right.rcon.f@outlook.com>
2025-01-04 22:23:18 +0900

    aa but slow

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use std::f64::INFINITY;
 
 use hittable::{Hittable, HittableList};
+use math::random_double;
 use ray::Ray;
 use sphere::Sphere;
 use vec3::{Color, Point3, Vec3};
 
+mod camera;
 mod hittable;
 mod math;
 mod print;
@@ -27,31 +29,27 @@ fn main() {
     const ASPECT_RATIO: f64 = 16.0 / 9.0;
     const IMAGE_WIDTH: i16 = 384;
     const IMAGE_HEIGHT: i16 = (IMAGE_WIDTH as f64 / ASPECT_RATIO) as i16;
+    const SAMPLES_PER_PIXEL: i32 = 100;
 
     println!("P3\n{} {}\n255", IMAGE_WIDTH, IMAGE_HEIGHT);
-
-    let viewport_height = 2.0;
-    let viewport_width = ASPECT_RATIO * viewport_height;
-    let focal_length = 1.0;
-
-    let origin = Point3::default();
-    let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
-    let vertical = Vec3::new(0.0, viewport_height, 0.0);
-    let lower_left_corner = origin - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0, 0.0, focal_length);
 
     let mut world: HittableList = HittableList::default();
     world.add(Box::new(Sphere::new(Point3::new(0.0, 0.0, -1.0), 0.5)));
     world.add(Box::new(Sphere::new(Point3::new(0.0, -100.5, -1.0), 100.0)));
 
+    let cam = camera::Camera::default();
+
     for j in (0..IMAGE_HEIGHT).rev() {
         eprintln!("\rScanlines remaining: {}", j);
         for i in 0..IMAGE_WIDTH {
-            let u = i as f64 / (IMAGE_WIDTH - 1) as f64;
-            let v = j as f64 / (IMAGE_HEIGHT - 1) as f64;
-            let r = Ray::new(origin, lower_left_corner + horizontal * u + vertical * v - origin);
-
-            let pixel_color = ray_color(&r, &world);
-            print::write_color(pixel_color);
+            let mut pixel_color = Color::default();
+            for _ in 0..SAMPLES_PER_PIXEL {
+                let u = (i as f64 + random_double()) / (IMAGE_WIDTH - 1) as f64;
+                let v = (j as f64 + random_double()) / (IMAGE_HEIGHT - 1) as f64;
+                let r = cam.get_ray(u, v);
+                pixel_color += ray_color(&r, &world);
+            }
+            print::write_color(pixel_color, SAMPLES_PER_PIXEL);
         }
     }
     eprintln!("\nDone.\n");
--- src/print.rs
+++ src/print.rs
@@ -1,10 +1,30 @@
 use crate::vec3::Color;
 
-pub fn write_color(pixel_color: Color) {
+pub fn write_color(pixel_color: Color, samples_per_pixel: i32) {
+    let mut r = pixel_color.x();
+    let mut g = pixel_color.y();
+    let mut b = pixel_color.z();
+
+    let scale = 1.0 / samples_per_pixel as f64;
+    r *= scale;
+    g *= scale;
+    b *= scale;
+
     println!(
         "{} {} {}",
-        (255.999 * pixel_color.x()) as i16,
-        (255.999 * pixel_color.y()) as i16,
-        (255.999 * pixel_color.z()) as i16
+        (255.999 * clamp(r, 0.0, 0.999)) as i16,
+        (255.999 * clamp(g, 0.0, 0.999)) as i16,
+        (255.999 * clamp(b, 0.0, 0.999)) as i16
     );
+}
+
+pub fn clamp(x: f64, min: f64, max: f64) -> f64 {
+    if x < min {
+        return min;
+    }
+    if x > max {
+        return max;
+    }
+
+    x
 }
